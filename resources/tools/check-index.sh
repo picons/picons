0edd4579ddd5d6b8ae48af7980152f8cf8273c8b
hotio <26902309+hotio@users.noreply.github.com>
2020-03-24 12:02:59 +0100

    improve fault checks

--- resources/tools/check-index.sh
+++ resources/tools/check-index.sh
@@ -6,5 +6,6 @@ style=$2
 sed -e 's/^.*=//g' $source_location/$style.index | sort -u | while read line ; do
     if [[ ! -f $source_location/logos/$line.default.png ]] && [[ ! -f $source_location/logos/$line.default.svg ]]; then
         echo The following logo does not exist: $line, found in $style.index
+        [[ -n $3 ]] && touch "$3"
     fi
 done
--- resources/tools/check-logos.sh
+++ resources/tools/check-logos.sh
@@ -5,15 +5,18 @@ cd $1
 for file in * ; do
     if [[ ! -f ${file%.*.*}.default.png ]] && [[ ! -f ${file%.*.*}.default.svg ]]; then
         echo The following logo has no default version: $file
+        [[ -n $2 ]] && touch "$2"
     fi
     
     if [[ $file == *.svg && ( -f ${file%.*.*}.default.png || -f ${file%.*.*}.light.png || -f ${file%.*.*}.dark.png || -f ${file%.*.*}.black.png || -f ${file%.*.*}.white.png ) ]]; then
         echo The following logo is an svg, but has one or more png alternatives: $file
+        [[ -n $2 ]] && touch "$2"
     fi
     
     if [[ $file == *.svg ]]; then
         if grep -q -e "</text>" -e "data:image/png" $file; then
             echo This svg contains text or a png image: $file
+            [[ -n $2 ]] && touch "$2"
         fi
     fi
 done
